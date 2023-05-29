f09b1552d75b4cbd274b918d5f1ec6d166610f24
Losarkreator <losarkreator@gmail.com>
2023-05-29 17:12:21 +0200

    Add circle time down

--- meditar/ViewModel/CountdownViewModel.swift
+++ meditar/ViewModel/CountdownViewModel.swift
@@ -7,28 +7,37 @@ import SwiftUI
 class CountdownViewModel: ObservableObject {
     @Published var currentNumber: Int
     var timer: Timer?
-
+    
+    private let startingNumber: Int
+    
     init(startingNumber: Int) {
+        self.startingNumber = startingNumber
         currentNumber = startingNumber
     }
-
+    
     func startCountdown() {
         guard currentNumber > 0 else {
             return
         }
-
+        
         timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] _ in
             guard let self = self else {
                 return
             }
-
             self.currentNumber -= 1
-
             if self.currentNumber == 0 {
                 self.timer?.invalidate()
                 self.timer = nil
             }
         }
     }
+    
+    func resetCountdown() {
+        timer?.invalidate()
+        timer = nil
+        currentNumber = startingNumber
+    }
+    
+    
+    
 }
-
--- meditar/Views/CountdownView.swift
+++ meditar/Views/CountdownView.swift
@@ -2,26 +2,56 @@
 //  meditar
 //  Created by Losark on 27/5/23.
 
-
 import SwiftUI
 
 struct CountdownView: View {
     @StateObject private var viewModel: CountdownViewModel
+    @State private var savedInitialValue: Int
+    @State private var isCountdownRunning = false
+
 
     init(startingNumber: Int) {
         _viewModel = StateObject(wrappedValue: CountdownViewModel(startingNumber: startingNumber))
+        _savedInitialValue = State(initialValue: startingNumber)
     }
 
     var body: some View {
         VStack {
-            Text("Countdown: \(viewModel.currentNumber)")
-                .font(.largeTitle)
-                .padding()
-
+            Spacer()
+            ZStack {
+                Circle()
+                    .stroke(lineWidth: 40)
+                    .opacity(0.3)
+                    .foregroundColor(.blue)
+                    .frame(width: 200.0, height: 200.0)
+                
+                //MARK: - ANIMACION
+                Circle()
+                    .trim(from: 0, to: CGFloat(viewModel.currentNumber) / CGFloat(savedInitialValue))
+                        .stroke(Color.azul, lineWidth: 40)
+                        .rotationEffect(.degrees(-90))
+                        .frame(width: 200, height: 200)
+                        .animation(.easeInOut)
+                
+                Text("\(viewModel.currentNumber)")
+                    .font(.largeTitle)
+                    .padding()
+                    .foregroundColor(.blue)
+            }
+            
+            Spacer()
+            
+            //MARK: - BOTON
             Button(action: {
+                if isCountdownRunning {
+                    viewModel.resetCountdown()
+                } else {
                 viewModel.startCountdown()
+                }
+                
+                isCountdownRunning.toggle()
             }) {
-                Text("Start Countdown")
+                Text(isCountdownRunning ? "Reiniciar" : "Empezar")
                     .font(.title)
                     .padding()
                     .background(Color.blue)

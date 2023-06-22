a1e92b73a8a3a5fcfbdda88d007a0f101a49e127
Losarkreator <losarkreator@gmail.com>
2023-06-22 11:44:44 +0200

    Previous instructions view

--- meditar/Views/Instructions/PreviousInstructionsView.swift
+++ meditar/Views/Instructions/PreviousInstructionsView.swift
@@ -9,49 +9,56 @@ struct PreviousInstructionsView: View {
     
     var body: some View {
         ZStack {
-            Color.morado.edgesIgnoringSafeArea(.vertical)
-            LinearGradient(gradient: Gradient(colors: [.negro.opacity(0.4), .morado]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+            Color.naranja.edgesIgnoringSafeArea(.vertical)
+            //LinearGradient(gradient: Gradient(colors: [.negro.opacity(0.4), .amarillo]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
             
             VStack {
                 TabView(selection: $currentPage) {
-                    VStack {
-                        Text("Beneficios de meditar")
-                            .font(.largeTitle)
-                            .lineLimit(1)
-                            .minimumScaleFactor(0.2)
-                            .fontWeight(.bold)
-                            .foregroundColor(Color.white)
-                            .shadow(color: .black.opacity(0.3), radius: 3, x: 1, y: 3)
-                        
-                        
-                        Spacer()
-
-                        
-                            Text("La meditación ha sido practicada durante milenios y se ha asociado con una amplia gama de beneficios para la salud y el bienestar.")
-                                .font(.system(size: 45))
-                                .minimumScaleFactor(0.5)
-                                .fontWeight(.heavy)
-                                .opacity(0.5)
-                                .foregroundColor(.blanco)
-//                            .background(.red)
-                        
-                        Spacer()
-                        
-                    }
-                    .padding(.horizontal, 30.0)
-                    .padding(.top, 30.0)
-                    .tag(0)
-                    //.background(.red.opacity(0.5))
+                    PreviousInstructionsSubView(title: "aPeacefulPlaceTitle_string", text: "aPeacefulPlaceDescription_string")
+                        .tag(0)
                     
-                    Text("Página 2")
+                    PreviousInstructionsSubView(title: "correctPostureTitle_string", text: "correctPostureDescription_string")
                         .tag(1)
-                    Text("Página 3")
-                        .tag(2)
                 }
                 .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
             }
             
         }
+    }
+}
+
+//MARK: - Views
+struct PreviousInstructionsSubView: View {
+    var title: String
+    var text: String
+    
+    var body: some View {
+        VStack {
+            Text(NSLocalizedString(title, comment: ""))
+                .font(.largeTitle)
+                .lineLimit(1)
+                .minimumScaleFactor(0.2)
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
+                .shadow(color: .black.opacity(0.3), radius: 3, x: 1, y: 3)
+            
+            Spacer(minLength: 40.0)
+            
+                Text(NSLocalizedString(text, comment: ""))
+                    .font(.system(size: 30))
+                    .minimumScaleFactor(0.5)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.blanco)
+//                            .background(.red)
+            
+            Spacer()
+            
+            //TODO: Add image
+            
+        }
+        .padding(.horizontal, 30.0)
+        .padding(.bottom, 40.0)
+        //                    .background(.red.opacity(0.5))
     }
 }
 
